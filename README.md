3d4be5c6991e0582a46ab186ed2b1603723950b6
JsBergbau <37013344+JsBergbau@users.noreply.github.com>
2021-02-07 23:17:14 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# TFACO2AirCO2ntrol_CO2Meter
+# Read TFA AirCO2ntrol Mini and CO2-Meter
+
+With this simple script you can read the CO2-Values of TFA AirCO2ntrol Mini https://www.tfa-dostmann.de/produkt/co2-monitor-airco2ntrol-mini-31-5006/ and CO2Meter https://www.co2meter.com/products/co2mini-co2-indoor-air-quality-monitor
+
+In most cases device is recognized as /dev/hidraw0
+
+Run the script with `sudo ./co2monitor.py /dev/hidraw0`
+
+This script is based on https://hackaday.io/project/5301-reverse-engineering-a-low-cost-usb-co-monitor/log/17909-all-your-base-are-belong-to-us and was ported to python3
+
+Issue once after insert `sudo chmod o+rw /dev/hidraw0` then you can simply run the script withou superuser permissions, which is prefered ./co2monitor.py /dev/hidraw0
+
+## Send the data of TFA AirCO2ntrol via MQTT
+
+You can send the data of this script via MQTT with this simple commandline
+`./co2monitor.py /dev/hidraw0 | grep --line-buffered -oP  "(?<=CO2:).*" | xargs -I % bash -c 'mosquitto_pub -h <IP of MQTT Server> -m % -t /CO2 ; echo %'`
---
+++ co2monitor.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3 -u
+
+import sys, fcntl, time
+
+def decrypt(key,  data):
+	cstate = [0x48,  0x74,  0x65,  0x6D,  0x70,  0x39,  0x39,  0x65]
+	shuffle = [2, 4, 0, 7, 1, 6, 5, 3]
+	
+	phase1 = [0] * 8
+	for i, o in enumerate(shuffle):
+		phase1[o] = data[i]
+	
+	phase2 = [0] * 8
+	for i in range(8):
+		phase2[i] = phase1[i] ^ key[i]
+	
+	phase3 = [0] * 8
+	for i in range(8):
+		phase3[i] = ( (phase2[i] >> 3) | (phase2[ (i-1+8)%8 ] << 5) ) & 0xff
+	
+	ctmp = [0] * 8
+	for i in range(8):
+		ctmp[i] = ( (cstate[i] >> 4) | (cstate[i]<<4) ) & 0xff
+	
+	out = [0] * 8
+	for i in range(8):
+		out[i] = (0x100 + phase3[i] - ctmp[i]) & 0xff
+	
+	return out
+
+def hd(d):
+	return " ".join("%02X" % e for e in d)
+
+if __name__ == "__main__":
+	# Key retrieved from /dev/random, guaranteed to be random ;)
+	key = [0xc4, 0xc6, 0xc0, 0x92, 0x40, 0x23, 0xdc, 0x96]
+
+	if (len(sys.argv) != 2):
+		print("Please specifiy the device, mostly it is /dev/hidraw0")
+		exit(1)
+	
+	fp = open(sys.argv[1], "a+b",  0)
+	
+	HIDIOCSFEATURE_9 = 0xC0094806
+	set_report = [0x00] + key
+	set_report = bytearray(set_report)
+	fcntl.ioctl(fp, HIDIOCSFEATURE_9, set_report)
+	
+	values = {}
+	
+	while True:
+		data = list(fp.read(8))
+		decrypted = decrypt(key, data)
+		if decrypted[4] != 0x0d or (sum(decrypted[:3]) & 0xff) != decrypted[3]:
+			 print (hd(data), " => ", hd(decrypted),  "Checksum error")
+		else:
+			 op = decrypted[0]
+			 val = decrypted[1] << 8 | decrypted[2]
+			
+			 values[op] = val
+			
+			 # Output all data, mark just received value with asterisk
+			 #print (", ".join( "%s%02X: %04X %5i" % ([" ", "*"][op==k], k, v, v) for (k, v) in sorted(values.items())), "  ",)
+			 ## From http://co2meters.com/Documentation/AppNotes/AN146-RAD-0401-serial-communication.pdf
+			 if 0x50 in values:
+				 print ("CO2: %4i" % values[0x50]) 
+			 if 0x42 in values:
+				 print ("T: %2.2f" % (values[0x42]/16.0-273.15))
+			 if 0x44 in values:
+				 print ("RH: %2.2f" % (values[0x44]/100.0))
+			 print
